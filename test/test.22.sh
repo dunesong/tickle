06899d7b1b702ab296b2fe9e8d0fad9e3c23f810
Jonathan Blake <jonathan@dunesong.com>
2017-09-14 11:22:53 -0700

    added debug option to test.22.sh

--- test/test.22.sh
+++ test/test.22.sh
@@ -5,7 +5,24 @@
 
 printf "test 22: should be able to tickle randomly (using statistical tests, failure if results are outside of 4-sigma) ... " 
 
+DEBUG=false
+while getopts ':d' opt; do
+    case $opt in
+    d)
+        DEBUG=true
+        ;;
+    \?)
+        echo "Invalid option: -$OPTARG" >&2
+        ;;
+    esac
+done
+
 results=$(echo "#tickle randomly interval 1 say Success" | ../tickle.py 2>&1)
+
+if [ "$DEBUG" == true ]; then
+    echo "$results"
+fi
+
 if [ "$results" != "Success" ]; then
     echo "failed"
     exit 1
@@ -14,6 +31,11 @@ fi
 trials_passed=0
 for i in $(seq 1 48); do
     results=$(echo "#tickle randomly interval 2 say Success" | ../tickle.py 2>&1)
+
+    if [ "$DEBUG" == true ]; then
+        echo "$results"
+    fi
+
     if [ "$results" == "Success" ]; then
         trials_passed=$((trials_passed +1))
     fi
